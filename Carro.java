ec17e9910b9b18affa4200b3f6fc851bb1cb7ee5
ErickMS99 <140689538+ErickMS99@users.noreply.github.com>
2023-12-15 19:42:48 -0300

    exercício modulo 7

---
+++ Carro.java
@@ -0,0 +1,23 @@
+public class Carro {
+
+	public String nome;
+	public int peso;
+	public String cor;
+	public int quantRodas;
+
+	public void ligar(){
+		//MÉTODO LIGAR
+	}
+
+	public void acelerar(){
+		//MÉTODO ACELERAR
+	}
+
+	public void freiar(){
+		//MÉTODO FREIAR
+	}
+
+	public void desligar(){
+		//MÉTODO desligar
+	}
+}
