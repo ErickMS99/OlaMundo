d32e45ac0633d8cbc14c98ee3cb39e6700344aa0
ErickMS99 <140689538+ErickMS99@users.noreply.github.com>
2024-01-03 19:51:37 -0300

    Exercício M9

---
+++ conta.java
@@ -0,0 +1,15 @@
+public class conta {
+
+    public static void main(String[] args) {
+        
+        long n1 = 10;
+
+        System.out.println(n1);
+
+        Long n2 = Long.valueOf(n1);
+
+        System.out.println(n2);
+        
+    }
+
+}
