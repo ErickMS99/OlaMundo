baa82290798966c05f2500e826f0b81c1bc69405
ErickMS99 <140689538+ErickMS99@users.noreply.github.com>
2024-01-04 21:56:14 -0300

    Exercício M10

---
+++ CalculoMedia2.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class CalculoMedia2 {
+    public static void main(String[] args){
+    
+        // Crie um objeto Scanner para a entrada do usuário4
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicite ao usuário para inserir quatro números
+        System.out.print("Digite o primeiro número: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        System.out.print("Digite o terceiro número: ");
+        double numero3 = scanner.nextDouble();
+
+        System.out.print("Digite o quarto número: ");
+        double numero4 = scanner.nextDouble();
+
+        // Calcule a média
+        double media = (numero1 + numero2 + numero3 + numero4) / 4;
+
+       // Valor total
+        double total = (numero1 + numero2 + numero3 + numero4); 
+        
+        //Exibir total
+        System.out.println("A soma total dos quatro numero é: " + total);
+
+        // Exiba o resultado
+        System.out.println("A média dos quatro números é: " + media);
+
+        // Fecha o Scanner 
+        scanner.close();
+
+        if(media >= 7){
+            System.out.println("aprovado");
+        } 
+        else if(media >= 5){
+            System.out.println("recuperação");
+        }
+        else if(media < 5){
+            System.out.println("reprovado");
+        } 
+    }
+}
